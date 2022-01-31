d861ab5bd66fb46d786f262e610a4dfe61e55214
Mongol666 <pisarevgerman78@gmail.com>
2022-01-31 14:36:39 +0300

    custom string qualifier

--- src/main/java/by/itacademy/gpisarev/spring/models/PrintedProduct.java
+++ src/main/java/by/itacademy/gpisarev/spring/models/PrintedProduct.java
@@ -1,10 +1,11 @@
 package by.itacademy.gpisarev.spring.models;
 
+import by.itacademy.gpisarev.spring.qualifiers.simple.BookQualifier;
+import by.itacademy.gpisarev.spring.qualifiers.string.TypeStringQualifier;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.Map;
 
@@ -18,7 +19,7 @@ public class PrintedProduct implements Printable {
     private Map<Integer, Author> authors;
 
     @Autowired
-    @Qualifier(value = "book-qualifier")
+    @TypeStringQualifier(name = "book-qualifier")
     private Type type;
 
     public PrintedProduct(int id, String name) {
