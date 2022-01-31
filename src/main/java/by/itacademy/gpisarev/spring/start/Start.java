578e4cf85df5d1167d0b31efe430e88844045df8
Mongol666 <pisarevgerman78@gmail.com>
2022-01-31 13:46:17 +0300

    injection by constructor and properties with p- and c-schemas

--- src/main/java/by/itacademy/gpisarev/spring/start/Start.java
+++ src/main/java/by/itacademy/gpisarev/spring/start/Start.java
@@ -1,6 +1,7 @@
 package by.itacademy.gpisarev.spring.start;
 
 import by.itacademy.gpisarev.spring.models.Author;
+import by.itacademy.gpisarev.spring.models.Type;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,10 +9,16 @@ public class Start {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        Author pushkin = context.getBean("pushkin", Author.class);
-        System.out.println(pushkin);
+        //Author pushkin = context.getBean("pushkin", Author.class);
+        //System.out.println(pushkin);
+        //Author lermontov = context.getBean("lermontov", Author.class);
+        //System.out.println(lermontov);
 
-        Author lermontov = context.getBean("lermontov", Author.class);
-        System.out.println(lermontov);
+        Type book = context.getBean("book", Type.class);
+        System.out.println(book);
+        Type magazine = context.getBean("magazine", Type.class);
+        System.out.println(magazine);
+        Type diary = context.getBean("diary", Type.class);
+        System.out.println(diary);
     }
 }
